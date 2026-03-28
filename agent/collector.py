b7801fb3a38ee9af36b90a5d9c6e6e61dbc30c43
Senku-qq <maksfloopy@gmail.com>
2026-03-28 18:41:37 +0100

    added collector function

--- agent/collector.py
+++ agent/collector.py
@@ -1,1 +1,12 @@
 # Логика захвата окон (pywin32)
+import win32gui
+import time 
+
+def test(): 
+    hwnd = win32gui.GetForegroundWindow()
+    title = win32gui.GetWindowText(hwnd)
+    print(title)
+
+if __name__ == "__main__":
+    time.sleep(3)
+    test() 
