dc3d1b9cef730865b27d2ad1accee95602b40026
Senku-qq <maksfloopy@gmail.com>
2026-03-30 22:50:47 +0200

    did beta version of IdleDetector class

--- agent/idle_detector.py
+++ agent/idle_detector.py
@@ -1,1 +1,25 @@
 # Логика pynput
+from pynput import keyboard , mouse
+import time 
+class IdleDetector: 
+    def __init__(self, threshold=120):
+        self.threshold = threshold
+        self.last_input_time = time.time()
+
+        self.mouse_listener = mouse.Listener(on_move=self.on_activity, on_click=self.on_activity , on_scroll=self.on_activity)
+        self.keyboard_listener = keyboard.Listener(on_press=self.on_activity, on_release=self.on_activity)
+
+        self.mouse_listener.start()
+        self.keyboard_listener.start()
+
+    def on_activity(self): 
+        self.last_input_time = time.time()
+    
+    def last_activity_time(self):
+        return time.time() - self.last_input_time 
+    
+    def is_idle(self): # если пользователь долго не двигается то отключаем сбор 
+        return self.last_activity_time() > self.threshold
+    
+
+    # надо будет сделать проверку на то, открыт ли сейчас плеер ютуб или что то где не нужна мышь и клавиатура 
