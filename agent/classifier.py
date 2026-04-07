1c9d79cc3e1d3ad69c1059082f95ec65b4d69414
Senku-qq <maksfloopy@gmail.com>
2026-04-07 22:28:56 +0200

    continue work with classifier.py

--- agent/classifier.py
+++ agent/classifier.py
@@ -1,6 +1,7 @@
 # Локальная первичная обработка строк
-
+from collections import deque
 def classified():
-    with open(file='/logs/logs.log', mode="r", encoding="UTF-8") as f: 
-        f.readline() # читать надо снизу
-        
+    with open(file=r'C:\Users\manyt\Desktop\Time-tracker\Auto-Time-Tracker\logs\logs.log', mode="r", encoding="UTF-8") as f: 
+        last_line = deque(f,maxlen=1) # читаем послеюнюю
+        print(last_line[0].strip('\n'))
+classified()
